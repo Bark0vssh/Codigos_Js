28c7c007afffa7c282eed499dd95f47094d400ed
Bark0vssh <hhhhh066652@gmail.com>
2025-07-31 21:16:07 -0300

    Primeiro Codigo

---
+++ Aula 01/atividade1.js
@@ -0,0 +1,9 @@
+var valor1 = 10
+var valor2 = 20
+var soma = valor1 + valor2
+var media = soma/2
+
+console.log("Valor 1: " + valor1)
+console.log("Valor 2: " + valor2)
+console.log("Soma: " + soma)
+console.log("Media:" + media)
